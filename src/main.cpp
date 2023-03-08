8edd88a19bd86f32dde1e952fa41728f592345a5
Andres Arana <3av.arana@gmail.com>
2023-03-07 23:23:22 -0400

    show window

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
-#include <iostream>
-#include <SDL2/SDL.h>
+#include <headers/game.h>
 
 int main()
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) > 0)
-    {
-        std::cout << "SDL_Init failed with error: " << SDL_GetError() << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    std::string greetings = "Hello, World\n";
-    std::cout << greetings << std::endl;
-
-    SDL_Quit();
+    Game game;
+    
+    game.run();
 
     return EXIT_SUCCESS;
 }
