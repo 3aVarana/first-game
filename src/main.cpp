311f63277cc49d30cd7724547e643f8688136ca4
Andres Arana <3av.arana@gmail.com>
2023-03-09 22:09:22 -0400

    Add validations before running app

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
 #include <headers/game.h>
 
 int main(int argc, char *argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) > 0)
-    {
-        std::cout<<"HEY.. SDL_Init HA ";
-    }
+        std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+    if (!(IMG_Init(IMG_INIT_PNG)))
+        std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
 
     Game game;
 
